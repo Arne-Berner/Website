8d1672cac08e6e356fbc3c7543259e10bc6983b7
Arne Berner <arne.berner@student.fh-kiel.de>
2025-07-09 15:46:46 +0200

    feat: trying some seo optimization with title and such

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is my first attempt at using yew with github pages.
+
+
+# Deployment
+I used MathiasYde's [deploy.yml](https://github.com/MathiasYde/jigsaw/blob/main/.github/workflows/deploy.yml) as a template.
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,109 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
+#[derive(Clone, PartialEq)]
+struct Video {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
+}
+
+// no weird reflection magic, name is not important
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>
+}
+
+// this will destructure the components to vidoelistprops and then into html
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+   let on_click = on_click.clone();
+   videos.iter().map(|video| {
+        let on_video_select = {
+            let on_click = on_click.clone();
+            let video = video.clone();
+            Callback::from(move |_| {
+                on_click.emit(video.clone())
+            })
+        };
+        html! {
+            <p key={video.id} onclick={on_video_select}>
+            {format!("{}: {}", video.speaker, video.title)}</p>
+        }
+    }).collect() 
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
     html! {
-    <>
-        <h1>{ "RustConf Explorer" }</h1>
         <div>
-            <h3>{"Videos to watch"}</h3>
-            <p>{ "John Doe: Building and breaking things" }</p>
-            <p>{ "Jane Smith: The development process" }</p>
-            <p>{ "Matt Miller: The Web 7.0" }</p>
-            <p>{ "Tom Jerry: Mouseless development" }</p>
-        </div>
-        <div>
-            <h3>{ "John Doe: Building and breaking things" }</h3>
+            <h3>{ video.title.clone() }</h3>
             <img src="https://placehold.co/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
         </div>
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
+
+    let selected_video = use_state(|| None);
+
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| {
+            selected_video.set(Some(video))
+        })
+    };
+
+    let details = selected_video.as_ref().map(|video| html! {
+        <VideoDetails video={video.clone()} />
+    });
+
+    html! {
+    <>
+        <head>
+            <title>Arne Berner</title>
+        </head>
+        <h1>{ "Arne Berner" }</h1>
+        <div>
+            <h3>{"Videos to watch"}</h3>
+                <VideosList videos={videos} on_click={on_video_select.clone()} />
+        </div>
+        { for details }
+    
     </>
 }
 }
