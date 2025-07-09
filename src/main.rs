b7ec514e597c36d7155cacd595066c4acef5e71d
Arne Berner <arne.berner@student.fh-kiel.de>
2025-07-09 15:54:14 +0200

    feat: added title again

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,7 @@ fn app() -> Html {
 
     html! {
     <>
+        <title>Schauspieler Arne Berner</title>
         <h1>{ "RustConf Explorer" }</h1>
         <div>
             <h2>{"Videos to watch"}</h2>
